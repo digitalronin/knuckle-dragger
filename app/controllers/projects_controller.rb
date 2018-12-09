55fa9216582f200a0a1dd6d38bcd1122ac14138c
David Salgado <david@digitalronin.com>
2018-12-09 13:50:11 +0000

    Display real issues instead of faker data
    This only affects the 'unassigned' issues in the
    main area of the page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,16 @@
 class ProjectsController < ApplicationController
   def create
+    repo = project_params.fetch(:url).sub('https://github.com/', '').strip
+    @issues = GithubRepo.issues(repo, :unassigned)
   end
 
   def index
     render :create
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:url)
   end
 end
