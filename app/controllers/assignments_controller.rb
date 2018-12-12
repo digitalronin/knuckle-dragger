7e89a870fb06a48fed7413961d7d1e6559899085
David Salgado <david@digitalronin.com>
2018-12-12 18:27:58 +0000

    Redirect back to project#show page after assign

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,9 @@
 class AssignmentsController < ApplicationController
   def create
-    repo = GithubRepo.new(assignment_params.fetch(:repo), session[:github_access_token])
-    repo.assign_issue(assignment_params)
+    repo_name = assignment_params.fetch(:repo)
+    github = GithubRepo.new(repo_name, session[:github_access_token])
+    github.assign_issue(assignment_params)
+    redirect_to project_path(repo_name)
   end
 
   def destroy
