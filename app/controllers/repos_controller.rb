bf2830c7977f960f4da179705775f0eeff5166bf
David Salgado <david@digitalronin.com>
2018-12-13 09:23:36 +0000

    Streamline the github oauth process
    Instead of waiting for the user to choose to 
    connect their github account, simply start the
    connection process as soon as they do something 
    which requires it.
    
    We stash the target repo name in the session, so
    we can redirect the user to the correct page when
    they have completed the authorisation step.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,7 +2,16 @@ class ReposController < ApplicationController
 
   def create
     full_name = repo_params.fetch(:url).sub('https://github.com/', '').strip
-    redirect_to repo_path(full_name)
+
+    if session[:github_access_token]
+      redirect_to repo_path(full_name)
+    else
+      # we need the user to connect their github account
+      # so stash the repo_full_name so we can redirect
+      # them to the repo_path afterwards
+      session[:repo_full_name] = full_name
+      redirect_to github_token_req_path
+    end
   end
 
   def show
